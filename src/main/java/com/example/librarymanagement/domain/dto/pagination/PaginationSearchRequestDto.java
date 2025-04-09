e7cd714f08399899fbb28dfd4fef450b92b07719
Tung <thanhtungph03.it@gmail.com>
2025-04-09 23:03:38 +0700

    fix PaginationDto

--- src/main/java/com/example/librarymanagement/domain/dto/pagination/PaginationSearchRequestDto.java
+++ src/main/java/com/example/librarymanagement/domain/dto/pagination/PaginationSearchRequestDto.java
@@ -3,24 +3,18 @@ package com.example.librarymanagement.domain.dto.pagination;
 import com.example.librarymanagement.constant.CommonConstant;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class PaginationFullRequestDto {
-
+public class PaginationSearchRequestDto extends PaginationRequestDto{
     @Parameter(description = "Keyword to search")
     private String keyword = CommonConstant.EMPTY_STRING;
 
     @Parameter(description = "Search by")
     private String searchBy = CommonConstant.EMPTY_STRING;
-
-    @Getter
-    @Parameter(description = "Active")
-    private Boolean activeFlag;
 
     public String getKeyword() {
         return keyword.trim();
