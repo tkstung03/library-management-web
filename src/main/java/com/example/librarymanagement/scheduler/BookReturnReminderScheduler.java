30b3a56d29670f2b4fd1708503ff15612bcd7c7d
tkstung03 <thanhtungph03.it@gmail.com>
2025-05-14 02:43:58 +0700

    fix BookReturnReminderScheduler

---
+++ src/main/java/com/example/librarymanagement/scheduler/BookReturnReminderScheduler.java
@@ -0,0 +1,58 @@
+package com.example.librarymanagement.scheduler;
+
+import com.example.librarymanagement.domain.dto.common.DataMailDto;
+import com.example.librarymanagement.domain.entity.BorrowReceipt;
+import com.example.librarymanagement.repository.BorrowReceiptRepository;
+import com.example.librarymanagement.util.SendMailUtil;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@Log4j2
+@RequiredArgsConstructor
+@Component
+public class BookReturnReminderScheduler {
+
+    private static final int maxOverdueDays = 3;
+
+    private final SendMailUtil sendMailUtil;
+
+    private final BorrowReceiptRepository borrowReceiptRepository;
+
+    @Scheduled(cron = "0 0 8 * * ?") // Chạy lúc 8:00 AM mỗi ngày
+    @Transactional
+    public void sendReminderEmails() {
+        LocalDate overdueThreshold = LocalDate.now().minusDays(maxOverdueDays);
+        List<BorrowReceipt> overdueRecords = borrowReceiptRepository.findOverdueRecords(overdueThreshold);
+
+        for (BorrowReceipt record : overdueRecords) {
+
+            Map<String, Object> properties = new HashMap<>();
+            properties.put("username", record.getReader().getFullName());
+            properties.put("receiptNumber", record.getReceiptNumber());
+            properties.put("returnDate", record.getDueDate());
+
+            DataMailDto mailDto = new DataMailDto();
+            mailDto.setTo(record.getReader().getEmail());
+            mailDto.setSubject("Nhắc nhở trả sách");
+            mailDto.setProperties(properties);
+
+            CompletableFuture.runAsync(() -> {
+                try {
+                    sendMailUtil.sendMailWithHTML(mailDto, "reminderEmail.html");
+                } catch (MessagingException e) {
+                    log.error("Error sending reminder email to {}", record.getReader().getEmail(), e);
+                }
+            });
+        }
+    }
+}
